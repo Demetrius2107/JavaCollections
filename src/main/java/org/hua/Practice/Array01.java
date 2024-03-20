383a0298dff30b54e1014e335d8b37e71619956e
ShuKun Li <2562017477@qq.com>
2024-03-20 17:08:43 +0800

    ArrayList 源码分析

---
+++ src/main/java/org/hua/Practice/Array01.java
@@ -0,0 +1,13 @@
+package org.hua.Practice;
+
+import java.util.ArrayList;
+
+public class Array01 {
+    public static void main(String[] args) {
+        ArrayList<String> listOfString = new ArrayList<>();
+        listOfString.add("null");
+        listOfString.add("zhao");
+        listOfString.add("java");
+        System.out.println(listOfString);
+    }
+}
